aae9b35b67574cbc89f7ac97b6c9de334b7c0dc9
Matt <mathieu.antonuklesperance@gmail.com>
2016-04-10 18:55:12 -0400

    Wildcard to include libs

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -40,8 +40,8 @@ export class ProjectConfig extends SeedConfig {
 
 
   (<any>this.SYSTEM_CONFIG_DEV.paths)['angular2-jwt'] =
-          `${this.APP_BASE}node_modules/angular2-jwt/angular2-jwt`;
+          `${this.APP_BASE}node_modules/angular2-jwt/*`;
   (<any>this.SYSTEM_CONFIG_DEV.paths)['angular2-logger'] =
-          `${this.APP_BASE}node_modules/angular2-logger/bundles/angular2-logger`;
+          `${this.APP_BASE}node_modules/angular2-logger/*`;
   }
 }
