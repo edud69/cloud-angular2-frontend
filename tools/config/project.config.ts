be86f1e75a1a5af6fe99bc4c236dd22eb9f1a1a9
Matt <mathieu.antonuklesperance@gmail.com>
2016-04-28 20:27:33 -0400

    Update path.

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -7,23 +7,21 @@ export class ProjectConfig extends SeedConfig {
 
 
   // API PATHS
-
-  // AUTH-SERVICE PATHS
   AUTHSERVICE_PROD_BASE_URL : string = 'https://region1.theshire.io/api/v1/auth/';
   AUTHSERVICE_DEV_BASE_URL : string = 'http://localhost:17501/';
-  // service paths
-  AUTHSERVICE_API_facebookLogin = this.authService('signin/facebook');
-  AUTHSERVICE_API_googleLogin = this.authService('signin/google');
-  AUTHSERVICE_API_login = this.authService('login');
-  AUTHSERVICE_API_refreshJwtToken = this.authService('token/refresh');
-  AUTHSERVICE_API_userSubscribe = this.authService('user/subscription');
-  AUTHSERVICE_API_userSubscribeConfirmation = this.authService('user/subscription/activation');
+  CHATSERVICE_WS_PROD_BASE_URL : string = 'wss://region1.theshire.io/ws/chat/connect';
+  CHATSERVICE_WS_DEV_BASE_URL : string = 'ws://localhost:17504/ws/connect';
+
+  // AUTH-SERVICE PATHS
+  AUTHSERVICE_API_facebookLogin = this._authService('signin/facebook');
+  AUTHSERVICE_API_googleLogin = this._authService('signin/google');
+  AUTHSERVICE_API_login = this._authService('login');
+  AUTHSERVICE_API_refreshJwtToken = this._authService('token/refresh');
+  AUTHSERVICE_API_userSubscribe = this._authService('user/subscription');
+  AUTHSERVICE_API_userSubscribeConfirmation = this._authService('user/subscription/activation');
 
   // CHAT-SERVICE PATHS
-  CHATSERVICE_WS_PROD_BASE_URL : string = 'wss://region1.theshire.io/ws/chat/connect';
-  CHATSERVICE_WS_DEV_BASE_URL : string = 'ws://localhost:17504/connect';
-  // service paths
-  CHATSERVICE_API_connect = this.chatServiceWS('');
+  CHATSERVICE_API_connect = this._chatServiceWS();
 
   constructor() {
     super();
@@ -42,11 +40,11 @@ export class ProjectConfig extends SeedConfig {
     this.NPM_DEPENDENCIES = seedDependencies.concat(additional_deps);
   }
 
-  private authService(path : string) : string {
+  private _authService(path : string) : string {
     return (this.ENV === 'prod' ? this.AUTHSERVICE_PROD_BASE_URL : this.AUTHSERVICE_DEV_BASE_URL) + path;
   }
 
-  private chatServiceWS(path : string) : string {
-    return (this.ENV === 'prod' ? this.CHATSERVICE_WS_PROD_BASE_URL : this.CHATSERVICE_WS_DEV_BASE_URL) + path;
+  private _chatServiceWS() : string {
+    return (this.ENV === 'prod' ? this.CHATSERVICE_WS_PROD_BASE_URL : this.CHATSERVICE_WS_DEV_BASE_URL);
   }
 }
