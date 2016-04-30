147043f3519ac00530dcc42187134b002629bb34
Matt <mathieu.antonuklesperance@gmail.com>
2016-04-30 18:35:35 -0400

    RELEASE CANDIDATE

--- src/client/app/+authentication/services/signin.service.ts
+++ src/client/app/+authentication/services/signin.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from 'angular2/core';
-import {Http, Headers, Response} from 'angular2/http';
+import {Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 
 import {HttpConstants} from '../../shared/index';
