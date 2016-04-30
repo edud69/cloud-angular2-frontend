e34cc5030659141d08cbe8d9935aeaa3b8939640
Matt <mathieu.antonuklesperance@gmail.com>
2016-04-30 16:54:39 -0400

    Basic auth

--- src/client/app/+authentication/services/signin.service.ts
+++ src/client/app/+authentication/services/signin.service.ts
@@ -19,21 +19,25 @@ export class SigninService {
    * Login.
    */
   login(username: string, password: string) {
-    // TODO pass something in the body, zuul does not like the fact that the body is empty...
-
     let headers : any = {};
     headers[HttpConstants.HTTP_HEADER_TENANTID] = 'master'; //TODO get current tenant
     headers[HttpConstants.HTTP_HEADER_ACCEPT] = HttpConstants.HTTP_HEADER_VALUE_APPLICATIONJSON;
     headers[HttpConstants.HTTP_HEADER_CONTENT_TYPE] = HttpConstants.HTTP_HEADER_VALUE_APPLICATIONJSON;
     headers[HttpConstants.HTTP_HEADER_AUTHORIZATION] =
-          HttpConstants.HTTP_HEADER_VALUE_BASIC_PREFIX + ' YWRtaW46YXNkZmFzZGY='; //TODO encode credentials
+          HttpConstants.HTTP_HEADER_VALUE_BASIC_PREFIX + ' ' + btoa(username + ':' + password);
 
     fetch('<%= AUTHSERVICE_API_login %>', {
       method: HttpConstants.HTTP_METHOD_POST,
       headers: headers
     })
       .then((response: any) => response.json())
-      .then((json: any) => this._authTokenService.updateToken(json))
+      .then((json: any) => {
+        if(json.status === HttpConstants.HTTP_STATUSCODE_UNAUTHORIZED) {
+          this._loggerService.info('Bad credentials used.'); 
+        } else {
+          this._authTokenService.updateToken(json);
+        }
+      })
       .catch((error: any) => this._loggerService.error('An error occurred. Trace: ' + error));
   }
 
