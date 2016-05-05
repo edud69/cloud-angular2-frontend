230212dcb15c3098434a1d835f3ec79b3f1c1f57
Mathieu Antonuk <mathieu.antonuk@biohiit.ca>
2016-05-05 11:24:28 +0000

    auth-token.service.ts edited online with Bitbucket

--- src/client/app/shared/services/authentication/auth-token.service.ts
+++ src/client/app/shared/services/authentication/auth-token.service.ts
@@ -100,7 +100,7 @@ export class AuthTokenService {
    */
   currentTenant() : string {
     return this._getTokenProperty(JwtConstants.JWT_TOKEN_PROPERTY_TID);
-  }  
+  }
 
   /**
    * Gets authorities.
